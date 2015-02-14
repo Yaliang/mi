497f87e15b2c40b518eb3ca9ab754d80b8014cf9
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-02-14 00:13:28 -0600

    change the moment start refreshing photo preview

--- script/functions.js
+++ script/functions.js
@@ -870,8 +870,6 @@ function refreshPreviewCanvas(){
 }
 
 function getMyProfile(){
-	refreshPreviewPhoto = true;
-	refreshPreviewCanvas();
 	var currentUser = Parse.User.current();
 	var owner = currentUser.getUsername();
 	var userId = currentUser.id;
@@ -901,7 +899,7 @@ function getMyProfile(){
 		$("#profile-edit-location").val(location);
 	} 
 	ParseUpdateCurrentUser(displayFunction, function(){});
-	displayFunction = function(object){
+	displayFunction = function(object, data){
 		var photo120 = object.get('profilePhoto120');
 		if (typeof(photo120) == "undefined") {
 			photo120 = "./content/png/Taylor-Swift.png";
@@ -912,7 +910,7 @@ function getMyProfile(){
 		image.src = photo120;
 		context.drawImage(image, 0, 0);
 	}
-	CacheGetProfilePhoto(userId, displayFunction);
+	CacheGetProfilePhoto(userId, displayFunction, {});
 }
 
 function saveProfile(){
