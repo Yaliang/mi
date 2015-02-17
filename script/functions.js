0c11e1ecc0d703776b15280fc530246c712e5dd5
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-02-16 21:59:22 -0600

    try to merge

--- script/functions.js
+++ script/functions.js
@@ -22,6 +22,7 @@ $(document).ready(function (){
 		if (window.navigator.standalone == true) {
 			//$('#message-content').trigger('blur');
 			sendToolbarActiveKeyboard('message-content');
+			
 		} else {
 			sendToolbarActiveKeyboard('message-content');
 		}
